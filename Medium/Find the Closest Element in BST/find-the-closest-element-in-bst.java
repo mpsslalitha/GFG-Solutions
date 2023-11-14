46aff88ca84f4cef3cc860db45137e0d4a7ed931
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2023-11-14 22:14:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the Closest Element in BST/find-the-closest-element-in-bst.java
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Solution T = new Solution();
+            System.out.println(T.minDiff(root,k));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+*/
+
+class Solution
+{
+    //Function to find the least absolute difference between any node
+    //value of the BST and the given integer
+    
+    static void f(Node root, int K, int ans){
+        if(root == null) return ;
+        
+        
+        int cur = Math.abs(root.data - K);
+        ans = Math.min(ans, cur);
+        f(root.left, K, ans);
+        f(root.right, K, ans);
+        
+    }
+    static int ans = Integer.MAX_VALUE;
+    static int minDiff(Node  root, int K) 
+    { 
+        // Write your code here
+        Node temp=root;
+        int ans=Integer.MAX_VALUE;
+        while(root!=null)
+        {
+            ans=Math.min(ans,Math.abs(root.data-K));
+            if(root.data>K)
+            root=root.left;
+            else if(root.data<K)
+            root=root.right;
+            
+            else
+            {
+                ans=0;
+                break;
+            }
+        }
+        return ans;
+    } 
+}
+
