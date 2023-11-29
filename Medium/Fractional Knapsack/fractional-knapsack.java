00ff1318e6ad478d7dd9e9fe1cea9bf02ad58cc2
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2023-11-29 16:44:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fractional Knapsack/fractional-knapsack.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+
+class GfG {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int w = Integer.parseInt(inputLine[1]);
+            Item[] arr = new Item[n];
+            inputLine = br.readLine().trim().split(" ");
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Item(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            System.out.println(String.format("%.2f", new Solution().fractionalKnapsack(w, arr, n)));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n) 
+    {
+        // Your code here
+        double ans = 0;
+        Arrays.sort(arr, (a, b) -> Double.compare((double)b.value/b.weight, (double)a.value/a.weight));
+
+        for (int i = 0; i < n; i++) {
+            if (W >= arr[i].weight) {
+                ans += arr[i].value;
+                W -= arr[i].weight;
+            } else if (W > 0) {
+                ans += (double)W * arr[i].value / arr[i].weight;
+                W = 0;
+            } else {
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+}
