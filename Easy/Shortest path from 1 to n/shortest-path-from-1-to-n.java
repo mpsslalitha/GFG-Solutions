973c2c43ebbfa00c7e6e49caa55952a64da98f6e
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2023-11-30 16:16:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Shortest path from 1 to n/shortest-path-from-1-to-n.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minStep(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    public int minStep(int n)
+    {
+        // code here
+        if(n == 1) return 0;
+        if(n%3 == 0) return 1+minStep(n/3);
+        else return 1+minStep(n-1);
+    }
+}
