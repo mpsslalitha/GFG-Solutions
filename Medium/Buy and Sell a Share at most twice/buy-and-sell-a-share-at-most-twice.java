88ee2cb614f8f1366524cc45987c05b0c0633776
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-02-23 21:15:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine().trim());
+        while(t-- > 0){
+            
+            int n; 
+            n = Integer.parseInt(br.readLine().trim());
+            
+            
+            int[] price = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.maxProfit(n, price);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    public static int maxProfit(int n, int[] price) {
+
+        // code here
+
+        int cost1=(int)(1e9),cost2=(int)(1e9),profit1=0,profit2=0;
+
+        for(int p:price){
+
+            cost1=Math.min(cost1,p);
+
+            profit1=Math.max(profit1,p-cost1);
+
+            cost2=Math.min(cost2,p-profit1);
+
+            profit2=Math.max(profit2,p-cost2);
+
+        }
+
+        return profit2;
+
+    }
+
+}      
