d4e79c457293df9b22f5e9f6d71cb6402a68f4ba
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-05-19 20:40:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the closest number/find-the-closest-number.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.findClosest(n, k, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findClosest(int n, int k, int[] arr) {
+        // code here
+        int smallest=0;
+        int sub;
+        int sub_real=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-k<0){
+                sub=(arr[i]-k)*(-1);
+            }
+            else{
+                sub=arr[i]-k;
+                
+            }
+            if(sub<sub_real){
+                sub_real=sub;
+                smallest=arr[i];
+                
+            }
+            if(sub==sub_real){
+                if(arr[i]>smallest){
+                    smallest=arr[i];
+                }
+            }
+        }
+        return smallest;
+    }
+}      
