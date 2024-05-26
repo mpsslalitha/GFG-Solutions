2a269507f2746c1876f9e64dcda418ec7371d707
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-05-26 21:04:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Cost To Make Two Strings Identical/minimum-cost-to-make-two-strings-identical.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String X = sc.next();
+            String Y = sc.next();
+            int costX = sc.nextInt();
+            int costY = sc.nextInt();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMinCost(X, Y, costX, costY));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int findMinCost(String x, String y, int costX, int costY) {
+        int common = longestCommonSubsequence(x,y,x.length(),y.length());
+        if(common==0){
+            return (x.length()*costX)+(y.length()*costY);
+        }
+        return ((x.length()-common)*costX)+((y.length()-common)*costY);
+    }
+    public static int longestCommonSubsequence(String str1, String str2,int n,int m){
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
