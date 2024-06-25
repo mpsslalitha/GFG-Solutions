2711daa4d3e909b361e377007c997abfd52df257
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-06-25 21:02:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            int n = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            int k = Integer.parseInt(S[2]);
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String S1[] = read.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = Integer.parseInt(S1[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans[][] = ob.rotateMatrix(k, mat);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for java
+
+class Solution {
+    int[][] rotateMatrix(int k, int mat[][]) {
+        k = k%mat[0].length;
+        for(int x[] : mat){
+            rotate(x, k);
+        }
+        return mat;
+    }
+    
+    void rotate(int[] arr, int k){
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+    }
+    
+    void reverse(int[] arr, int i, int j){
+        while(i<=j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++; j--;
+        }
+    }
+}
