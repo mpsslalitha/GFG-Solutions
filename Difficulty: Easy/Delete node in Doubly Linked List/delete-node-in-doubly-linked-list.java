ad67133d4bf2c1d5038f230b7e2f4938248fe247
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-06-30 13:55:37 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = null;
+            Node tail = head;
+
+            for (int i = 0; i < n; i++) {
+                int temp = sc.nextInt();
+                if (head == null) {
+                    head = new Node(temp);
+                    tail = head;
+                } else {
+                    Node newNode = new Node(temp);
+                    tail.next = newNode;
+                    newNode.prev = tail;
+                    tail = newNode;
+                }
+            }
+            int x = sc.nextInt();
+
+            Solution obj = new Solution();
+            Node res = obj.deleteNode(head, x);
+
+            Node.printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for doubly Link List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        //Write code here.
+        Node temp=head;
+        Node temp1=null;
+        if(x==1){
+            head=temp.next;
+            temp.next.prev=null;
+            temp.next=null;
+          
+            return head;
+        }
+        
+        while(temp.next!=null && x>1){
+            temp1=temp;
+            temp=temp.next;
+            x--;
+        }
+        if(temp.next==null){
+            temp1.next=null;
+            return head;
+        }
+        
+        temp1.next=temp.next;
+        temp.next.prev=temp1;
+        return head;
+    }
+}
