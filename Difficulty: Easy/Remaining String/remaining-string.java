9ddbbf901905be625673832f4e7ade6e1e6627ce
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-07-16 10:54:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            char ch = sc.next().charAt(0);
+            int count = sc.nextInt();
+            Solution ob = new Solution();
+            String result = ob.printString(s, ch, count);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public String printString(String s, char ch, int count) {
+        String res="";
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==ch){
+                c++;
+            }
+            if(c==count && i+1 <s.length()){
+                return s.substring(i+1);
+            }
+        }
+        
+        return res;
+    }
+}
