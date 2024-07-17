5ff83e236f6f293db15f8b30e4787756a7a7f4de
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-07-17 14:30:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Construct Binary Tree from Parent Array/construct-binary-tree-from-parent-array.java
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+
+class CreateTree {
+    static ArrayList<Integer> result = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+            Solution ob = new Solution();
+            Node root = ob.createTree(arr);
+
+            printLevelOrder(root);
+            System.out.println();
+        }
+    }
+
+    public static void printList(Node root) {
+        while (root != null) {
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void printLevelOrder(Node root) {
+        int h = height(root);
+        int i;
+        for (i = 1; i <= h; i++) {
+            result.clear();
+            printGivenLevel(root, i);
+            Collections.sort(result);
+            for (int j = 0; j < result.size(); j++)
+                System.out.print(result.get(j) + " ");
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
+
+        else {
+
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+
+            if (lheight > rheight)
+                return (lheight + 1);
+            else
+                return (rheight + 1);
+        }
+    }
+
+    public static void printGivenLevel(Node node, int level) {
+        if (node == null) return;
+        if (level == 1)
+            result.add(node.data);
+        else if (level > 1) {
+            printGivenLevel(node.left, level - 1);
+            printGivenLevel(node.right, level - 1);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*node class of the binary tree
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class Solution {
+    // Function to construct binary tree from parent array.
+    public Node createTree(int parent[]) {
+        // Your code here
+        int n = parent.length;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        Node[] arr = new Node[n];
+        
+        if(n == 0) return null;
+        
+        for(int i = 0; i < n; i++)
+        {
+            hm.put(i,2);
+            arr[i] = new Node(i);
+        }
+        Node root = null;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(parent[i] != -1)
+            {
+                if(hm.get(parent[i]) == 2)
+                {
+                    arr[parent[i]].left = arr[i];
+                    hm.put(parent[i],1);
+                }
+                else
+                {
+                    arr[parent[i]].right = arr[i];
+                }
+            }
+            else
+            {
+                root = arr[i];
+            }
+        }
+        
+        return root;
+    }
+}
+
