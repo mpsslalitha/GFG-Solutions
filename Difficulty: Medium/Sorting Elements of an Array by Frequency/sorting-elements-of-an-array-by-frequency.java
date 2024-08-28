d26ad07d8a14ebfbf0828d81edce98eb5c52e5b1
Mulukutla Poojitha Sai Sri Lalitha <115095336+mpsslalitha@users.noreply.github.com>
2024-08-28 20:55:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to sort the array according to frequency of elements.
+    public ArrayList<Integer> sortByFreq(int arr[]) {
+        // add your code here
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        ArrayList<Integer>list=new ArrayList<>(map.keySet());
+        Collections.sort(list,(a,b)->{
+            if(map.get(a)==map.get(b)){
+                return a-b;
+            }
+            else{
+                return map.get(b)-map.get(a);
+            }
+        });
+        ArrayList<Integer>result=new ArrayList<>();
+        for(int key:list){
+            for(int i=0;i<map.get(key);i++){
+                result.add(key);
+            }
+        }
+        return result;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Driverclass {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine());
+        while (n != 0) {
+            String input = sc.readLine();
+            String[] inputs = input.split(" ");
+            int[] arr = new int[inputs.length];
+
+            for (int i = 0; i < inputs.length; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
+            }
+
+            ArrayList<Integer> ans = new ArrayList<Integer>();
+            ans = new Solution().sortByFreq(arr);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+            n--;
+        }
+    }
+}
+
+// } Driver Code Ends
